642bbd77f5ae601f44c6e49e2193127ee8986424
Fabio Tablet <mpc.fabio@gmail.com>
2015-12-13 16:15:55 -0200

    Inicio do Projeto de Testes

---
+++ Santander.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Impactro.Cobranca;
+using Impactro.Layout;
+using System.IO;
+
+namespace Test
+{
+    [TestClass]
+    public class Santander
+    {
+        const string fileTest = @"..\..\TXT\Remessa_Santander.txt"; // para deixar na pasta TXT/ do projeto
+
+        CedenteInfo Cedente;
+
+        [TestInitialize]
+        public void Init()
+        {
+            Cedente = new CedenteInfo();
+            Cedente.Banco = "033";
+            Cedente.Agencia = "1234-1";
+            Cedente.Conta = "001234567-8";
+            Cedente.CodCedente = "1231230";
+            Cedente.CarteiraTipo = "5";
+            Cedente.Carteira = "101";
+            Cedente.CedenteCOD = "33333334892001304444"; // 20 digitos (note que o final, é o numero da conta, sem os ultios 2 digitos)
+            Cedente.Convenio = "0000000000000000002222220"; // 25 digitos
+            Cedente.Cedente = "TESTE QUALQUER LTDA";
+            Cedente.CNPJ = "88.083.264/0001-05";
+            Cedente.useSantander = true; //importante para gerar o código de barras correto (por questão de compatibilidade o padrão é false)
+        }
+
+        [TestMethod, TestCategory("Remessa")]
+        public void Remessa_Santander()
+        {
+            LayoutBancos lb = new LayoutBancos();
+            lb.Init(Cedente);
+            Util.AddBoletos(lb);
+            string txt = lb.Remessa();
+            Console.Write(txt);
+            // File.WriteAllText(fileTest, txt); // Gera um novo modelo
+            string cAnterior = File.ReadAllText(fileTest);
+
+            // Isso necessáriamente não é um erro, pode ter sido uma correção ou melhoria que agora contemple mais casos
+            Assert.IsTrue(cAnterior == txt, "O resultado da remessa mudou");
+        }
+
+        [TestMethod, TestCategory("Retorno")]
+        public void Retorno_Santander()
+        {
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,54 @@
+﻿using Impactro.Cobranca;
+using Impactro.Layout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    internal class Util
+    {
+
+        const int NossoNumeroInicial = 1000;
+        const double ValorInicial = 1000d;
+        static DateTime VencimentoInicial = DateTime.Parse("20/10/2015");
+
+        static internal void AddBoletos(LayoutBancos lb)
+        {
+            //Definição dos dados do sacado
+            SacadoInfo Sacado = new SacadoInfo();
+            Sacado.Sacado = "Fábio F S";
+            Sacado.Documento = "922.719.724-93";
+            Sacado.Endereco = "rua qualquer lugar, S/N";
+            Sacado.Cidade = "São Paulo";
+            Sacado.Bairro = "Centro";
+            Sacado.Cep = "55555-333";
+            Sacado.UF = "SP";
+            Sacado.Email = "email@provedor.com";
+            Sacado.Avalista = "Avalista";
+
+            for (int n = 0; n < 10; n++)
+            {
+                //Definição das Variáveis do boleto
+                BoletoInfo Boleto = new BoletoInfo();
+                Boleto.BoletoID = n;
+                Boleto.NossoNumero = (NossoNumeroInicial + n).ToString();
+                Boleto.NumeroDocumento = Boleto.NossoNumero;
+                Boleto.ValorDocumento = ValorInicial + n;
+                Boleto.DataDocumento = DateTime.Now;
+                Boleto.DataVencimento = VencimentoInicial.AddDays(n);
+                Boleto.Instrucoes = "Todas as informações deste bloqueto são de exclusiva responsabilidade do cedente";
+
+                // outros campos opcionais
+                Boleto.ValorMora = Boleto.ValorDocumento * 0.2 / 30; // Vale lembrar que o juros pode ser tão pequeno que as vezes pode sair como isento
+                Boleto.PercentualMulta = 0.03;
+                Boleto.ValorDesconto = n;
+                Boleto.DataDesconto = DateTime.Now;
+                Boleto.ValorOutras = -n; // abatimentos 
+
+                lb.Add(Boleto, Sacado);
+            }
+        }
+    }
+}
