eff74b4db8588ed8348d732135170100af580594
Fabio Talua <mpc.fabio@gmail.com>
2016-06-19 21:22:37 -0300

    Teste BB

--- BB.cs
+++ BB.cs
@@ -9,6 +9,47 @@ namespace Test
     [TestClass]
     public class BB
     {
+        const string fileTest = @"..\..\TXT\Remessa_BB.txt"; // para deixar na pasta TXT/ do projeto
+
+        CedenteInfo Cedente;
+
+        [TestInitialize]
+        public void Init()
+        {
+            Cedente = new CedenteInfo();
+            Cedente.Cedente = "TESTE QUALQUER LTDA";
+            Cedente.CNPJ = "88.999.222/0001-33";
+            Cedente.Banco = "001";
+            Cedente.Agencia = "1234-5";
+            Cedente.Conta = "56785678-1";
+            Cedente.Carteira = "16";
+            Cedente.Modalidade = "18";
+            Cedente.Convenio = "123456";
+        }
+
+        [TestMethod, TestCategory("Remessa")]
+        public void Remessa_BB()
+        {
+            LayoutBancos lb = new LayoutBancos();
+            lb.Init(Cedente, LayoutTipo.CNAB400);
+            lb.DataHoje = DateTime.Parse("19/06/2016 20:31:08");
+
+            Util.AddBoletos(lb);
+
+            // Exibir as informações de DUMP ajuda a char os erros e diferenças
+            // lb.ShowDumpLine = true;
+
+            string txt = lb.Remessa();
+            Console.Write(txt);
+
+            File.WriteAllText(@"..\..\TXT\Teste_BB.txt", txt); // Gera um arquivo para testes de compraração
+            // File.WriteAllText(fileTest, txt); // Gera um novo modelo
+            string cAnterior = File.ReadAllText(fileTest);
+
+            // Isso necessáriamente não é um erro, pode ter sido uma correção ou melhoria que agora contemple mais casos
+            Assert.IsTrue(cAnterior == txt);
+        }
+
         [TestMethod, TestCategory("CampoLivre")]
         public void CampoLivre_BB()
         {
